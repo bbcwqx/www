ba5aaf3417e1397100c47864f7cf83a29c6d8204
bbcwqx <219390702+bbcwqx@users.noreply.github.com>
2025-08-10 17:38:15 -0400

    Add explicit grid item aspect ratios

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,42 +3,42 @@ import { define } from "../utils.ts";
 export default define.page(function Home() {
   return (
     <div class="mx-auto">
-      <div class="grid grid-cols-3 lg:grid-cols-6 gap-4 auto-rows-fr">
+      <div class="grid grid-cols-3 md:grid-cols-6 gap-4">
         <img
-          class="rounded"
+          class="rounded aspect-square object-cover"
           src="https://cdn.bsky.app/img/avatar/plain/did:plc:rcjhtxh5v4mwvrbezap3hixf/bafkreib5f2vynlmo3aa6napzkhbrykkpmxwz7uks3j5h244okbax5e6xp4@jpeg"
         />
-        <div class="flex items-center justify-center bg-amber-300 dark:bg-amber-500 rounded">
+        <div class="aspect-square flex items-center justify-center bg-amber-300 dark:bg-amber-500 rounded">
           <p class="text-3xl font-extrabold">tile 1</p>
         </div>
-        <div class="flex items-center justify-center bg-amber-400 dark:bg-amber-600 rounded">
+        <div class="aspect-square flex items-center justify-center bg-amber-400 dark:bg-amber-600 rounded">
           <p class="text-3xl font-extrabold">tile 2</p>
         </div>
-        <div class="col-span-3 bg-[#f8f8fa] dark:bg-[#1c1c1e] rounded flex items-center">
+        <div class="col-span-3 aspect-[3/1] bg-[#f8f8fa] dark:bg-[#1c1c1e] rounded flex items-center overflow-hidden p-2">
           <iframe
-            class="rounded-xl w-full overflow-hidden"
+            class="w-full [clip-path:inset(1px_round_12px)] scale-75 lg:scale-100"
             allow="autoplay *; encrypted-media *; fullscreen *; clipboard-write"
             sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
             src="https://embed.music.apple.com/us/song/zissou/1825870058?theme=auto"
           >
           </iframe>
         </div>
-        <div class="col-span-3 row-span-2 flex items-center justify-center bg-pink-300 dark:bg-pink-600 rounded">
+        <div class="col-span-3 row-span-2 aspect-[3/2] flex items-center justify-center bg-pink-300 dark:bg-pink-600 rounded">
           <p class="text-3xl font-extrabold">tile 3</p>
         </div>
-        <div class="flex items-center justify-center bg-pink-400 dark:bg-pink-700 rounded">
+        <div class="aspect-square flex items-center justify-center bg-pink-400 dark:bg-pink-700 rounded">
           <p class="text-3xl font-extrabold">tile 4</p>
         </div>
-        <div class="col-span-2 flex items-center justify-center bg-pink-500 dark:bg-pink-800 rounded">
+        <div class="col-span-2 aspect-[2/1] flex items-center justify-center bg-pink-500 dark:bg-pink-800 rounded">
           <p class="text-3xl font-extrabold">tile 5</p>
         </div>
-        <div class="flex items-center justify-center bg-rose-300 dark:bg-rose-500 rounded">
+        <div class="aspect-square flex items-center justify-center bg-rose-300 dark:bg-rose-500 rounded">
           <p class="text-3xl font-extrabold">tile 6</p>
         </div>
-        <div class="flex items-center justify-center bg-rose-400 dark:bg-rose-600 rounded">
+        <div class="aspect-square flex items-center justify-center bg-rose-400 dark:bg-rose-600 rounded">
           <p class="text-3xl font-extrabold">tile 7</p>
         </div>
-        <div class="flex items-center justify-center bg-rose-500 dark:bg-rose-700 rounded">
+        <div class="aspect-square flex items-center justify-center bg-rose-500 dark:bg-rose-700 rounded">
           <p class="text-3xl font-extrabold">tile 8</p>
         </div>
       </div>
