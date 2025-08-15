8c2a4bd73d4ef2d161ccc246ff7fe79d7d5d03a3
bbcwqx <219390702+bbcwqx@users.noreply.github.com>
2025-08-15 00:48:55 -0400

    Add github actions workflow

---
+++ tests/server_test.ts
@@ -0,0 +1,20 @@
+import { assertEquals, assertExists } from "jsr:@std/assert";
+
+Deno.test("Serve", async () => {
+  const serverProcess = new Deno.Command(Deno.execPath(), {
+    args: ["task", "start"],
+  }).spawn();
+
+  await new Promise((resolve) => setTimeout(resolve, 200));
+
+  try {
+    const response = await fetch("http://localhost:8000");
+    assertEquals(response.status, 200);
+
+    const body = await response.text();
+    assertExists(body);
+  } finally {
+    serverProcess.kill();
+    await serverProcess.status;
+  }
+});
